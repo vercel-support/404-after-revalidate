bf43d735c15ad1eff25e5d872f3b204817585ae0
robinvdv12 <ro.vdv@hotmail.com>
2022-03-01 19:49:31 +0100

    removed home link

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,6 @@ const Page: NextPage<PropsType> = (props): JSX.Element => {
           margin: "auto",
         }}
       >
-        <Link href="/">Home</Link>
         {props.menuItems.map(({ menuItem }) => (
           <Link href={getPathname(menuItem.url)}>{menuItem.title}</Link>
         ))}
